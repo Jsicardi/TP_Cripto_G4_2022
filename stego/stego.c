a0214cbfa37ae7b7766ddf2a946e4e49232d04e5
JSicardi <jsicardi@itba.edu.ar>
2022-06-25 13:58:18 -0300

    Fix LSBI embed algorithm

--- stego/stego.c
+++ stego/stego.c
@@ -90,7 +90,6 @@ bool get_lsbi_pattern(uint8_t byte, uint8_t *pattern){
     get_bit_at(byte,6,&sec_lsb);
     get_bit_at(byte,5,&third_lsb);
 
-    //printf("2nd LSB: %d 3rd LSB: %d\n",sec_lsb,third_lsb);
     set_bit_at(pattern,7,sec_lsb);
     set_bit_at(pattern,6,third_lsb);
     return true;
@@ -120,7 +119,7 @@ void get_inversion_functions(BmpBody body,BinaryMessage *msg, FILE * origin_fd,u
         if(!read_next_bit(&message_bit,&aux_msg)){
             break;
         }
-        //printf("LSB: %d Pattern: %d Message LSB: %d\n",lsb,pattern,message_lsb);
+;
         if(message_bit == lsb){
             modified_pixels_matrix[pattern][1]++;
         }
@@ -130,7 +129,6 @@ void get_inversion_functions(BmpBody body,BinaryMessage *msg, FILE * origin_fd,u
     }
 
     for(uint32_t j = 0; j < 4; j++){
-        printf("Pattern: %d, Modified bits: %d Conserved bits: %d\n",j,modified_pixels_matrix[j][0],modified_pixels_matrix[j][1]);
         if(modified_pixels_matrix[j][0] > modified_pixels_matrix[j][1]){
             pattern_functions[j] = &bit_inversion;
             set_bit_at(inversion_byte,4+j,1);
@@ -160,19 +158,23 @@ bool invert_lsbi_message_bits(BmpBody body,BinaryMessage *msg, FILE * origin_fd,
     uint8_t read_byte;
     uint8_t message_bit;
     uint8_t pattern;
-    uint8_t bit_position = 0; 
-    uint8_t msg_index = 0;
+    uint8_t bit_position = 0;
+
     for(uint32_t i = 0; i < body.pixel_count * 3; i++){
         read_x_bytes(&read_byte,1,origin_fd);
         get_lsbi_pattern(read_byte,&pattern);
         if(!read_next_bit(&message_bit,&aux_msg)){
             break;
         };
-        set_bit_at(msg->curr_byte_ptr + msg_index, bit_position,pattern_functions[pattern](message_bit));
+        if(bit_position == BITS_IN_BYTE-1){
+            set_bit_at(aux_msg.curr_byte_ptr - 1,bit_position,pattern_functions[pattern](message_bit));
+        }
+        else{
+            set_bit_at(aux_msg.curr_byte_ptr,bit_position,pattern_functions[pattern](message_bit));
+        }
         bit_position++;
         if(bit_position == BITS_IN_BYTE){
             bit_position = 0;
-            msg_index++;
         } 
     }
 
